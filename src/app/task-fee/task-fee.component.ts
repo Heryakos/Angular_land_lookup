615f93b06ec4b51bbc1ee8dfff93fec0964b4e42
Heryakos Meles <heryakosmeles@gmail.com>
2023-10-23 03:52:22 -0900

    fix

--- src/app/task-fee/task-fee.component.ts
+++ src/app/task-fee/task-fee.component.ts
@@ -11,6 +11,7 @@ import { MessageService } from 'primeng/api';
 
 })
 export class TaskFeeComponent implements OnInit {
+  public isDropdownSelected: boolean = false;
   displayBasic:boolean=false;
   showSuccess: boolean = false;
   public task_ID: any;
@@ -62,27 +63,6 @@ showSuccessMessage() {
     updated_Date: new FormControl(new Date().toISOString().substr(0, 10)),
     deleted_Date: new FormControl(new Date().toISOString().substr(0, 10)),
     is_Deleted: new FormControl(new Date().toISOString().substr(0, 10)),
-    // created_Date: new FormControl(new Date().toISOString().substr(0, 10)),
-    // licence_type: new FormControl(),
-    // file_no: new FormControl(),
-    // site_ID: new FormControl(),
-    // mineral_ID: new FormControl(),
-    // trade_Nname: new FormControl(),
-    // renwal_date: new FormControl(new Date().toISOString().substr(0, 10)),
-    // licence_fee: new FormControl(),
-    // cadastral_fee: new FormControl(),
-    // status_of_License: new FormControl(),
-    // tin_Certificate: new FormControl(),
-    //   application:new FormControl(),
-    //   feasibility: new FormControl(),
-    //   agreement:new FormControl(),
-    //   certificate: new FormControl(),
-    //   map: new FormControl(),
-    //   work_program: new FormControl(),
-    // license_code: new FormControl(),
-    // coordinate: new FormControl(),
-    // selct_mineral: new FormControl(),
-    // select_site: new FormControl(),
     
   })
   ngOnInit(): void {
@@ -93,7 +73,6 @@ showSuccessMessage() {
       id: generateGuid(),
       task_ID:generateGuid(),
       task_code:generateGuid(),
-      // service_ID: generateGuid(),
       created_by: generateGuid(),
       updated_By: generateGuid(),
       deleted_By: generateGuid(),
@@ -137,13 +116,13 @@ showSuccessMessage() {
   }  
   openAddForm() {
     this.button = 'Save';
-    // this.form.reset({
-    //   id:generateGuid(),
-      // service_ID: generateGuid(),
-    //   task_ID: generateGuid(),
-    // })
     this.taskName = null; 
   } 
+  onServiceIDChange(value: any) {
+    this.isDropdownSelected = !!value;
+    
+    this.passdata(value);
+  }
   passdata(data:any){
     console.log('dataa',data);
     
@@ -163,24 +142,6 @@ onFormSubmit() {
     this.messageService.add({ severity: 'error', summary: 'Error Message', detail: 'Failed to add table' });
   }
 );
-//     console.log('taskname',this.form.get('task_Name')?.value);
-//     if(this.taskName==null||this.taskName==undefined){  
-//     if(this.form.get('task_Name')?.value!=null||this.form.get('task_Name')?.value!=undefined){
- 
-// } else {
-//    this.messageService.add({ severity: 'error', summary: 'Error Message', detail: 'You have not filled in the task fee' });
-//   }
-//       }else{
-//       this._empService.taskfeeupdate(this.form.value).subscribe((res)=>{
-//       this.taskfeeList();
-//       this.messageService.add({severity:'success', summary: 'Success Message', detail:'Table Updated successfully'});
-
-//     },
-//     (error) => {
-//       this.messageService.add({ severity: 'error', summary: 'Error Message', detail: 'Failed to update table' });
-//     }
-//   );
-//     }
 }
   taskfeeList() {
     this._empService.taskfeeList().subscribe({
