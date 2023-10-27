87378c159d52af1ae5e7653fe9b683042170abd8
Heryakos Meles <heryakosmeles@gmail.com>
2023-10-27 03:53:48 -0900

    adding routing and environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  username: window['username' as any],
+  rootpath: window['rootpath' as any],
+  rootPath2:window["rootPath2" as any],
+  rootpath6:window['rootpath6' as any],
+  rootPath3:window["rootPath3" as any],
+  formPath: window["formPath" as any],
+  rootpathh: window["rootpathh" as any],
+  rootpath4: window["rootpath4" as any],
 };
 
 /*
