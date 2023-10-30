8defe6752688c56fc57f8787060d1c82f1bf7ba2
Heryakos Meles <heryakosmeles@gmail.com>
2023-10-29 20:49:52 -0900

    Fixing Environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   production: false,
-  username: window['username' as any],
-  rootpath: window['rootpath' as any],
-  rootPath2:window["rootPath2" as any],
-  rootpath6:window['rootpath6' as any],
-  rootPath3:window["rootPath3" as any],
-  formPath: window["formPath" as any],
+  // username: window['username' as any],
+  // rootpath: window['rootpath' as any],
+  // rootPath2:window["rootPath2" as any],
   rootpathh: window["rootpathh" as any],
-  rootpath4: window["rootpath4" as any],
+  rootpath6:window['rootpath6' as any],
+  // rootPath3:window["rootPath3" as any],
+  // formPath: window["formPath" as any],
+  // rootpath4: window["rootpath4" as any],
 };
 
 /*
