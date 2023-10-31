5424f7cdf7a83c96c4412e1bc0d169e65d8fc900
Heryakos Meles <heryakosmeles@gmail.com>
2023-10-31 04:08:27 -0900

    Fix some bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   displayFlags: {[key: string]: boolean} = {};
 
   constructor() {
     this.initializeDisplayFlags();
+  }
+
+  ngOnInit() {
+    // Retrieve the active button from local storage
+    const activeButton = localStorage.getItem('activeButton');
+    if (activeButton) {
+      this.showDisplay(activeButton);
+    }
   }
 
   initializeDisplayFlags() {
@@ -29,5 +37,8 @@ export class AppComponent {
     for (const key in this.displayFlags) {
       this.displayFlags[key] = key === property;
     }
+
+    // Store the active button in local storage
+    localStorage.setItem('activeButton', property);
   }
 }
